149cb2938959aab275a151322c22f49b655e98af
yijordan <yij@uchicago.edu>
2025-02-07 03:16:55 +0000

    added reloader

--- dice.rb
+++ dice.rb
@@ -1,8 +1,12 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
   "Hello World"
 end
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+get("/asdf") do
+  "Does it work?"
 end
