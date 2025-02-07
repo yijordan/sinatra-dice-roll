7a43e28623f76eb666f98308502620325aff5b68
yijordan <yij@uchicago.edu>
2025-02-07 03:24:03 +0000

    added 1d6

--- dice.rb
+++ dice.rb
@@ -10,3 +10,18 @@ end
 get("/asdf") do
   "Does it work?"
 end
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+get("/dice/1/6") do
+  die = rand(1..6)
+  outcome = "You rolled a #{die}."
+  "<h1>1d6</h1>
+  <p>#{outcome}</p>"
+end
