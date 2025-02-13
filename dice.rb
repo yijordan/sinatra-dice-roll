0ce26324eef788983588a2bf292350c241c6fd7e
yijordan <yij@uchicago.edu>
2025-02-13 21:58:53 +0000

    made view templates for 1d6 and 2d6

--- dice.rb
+++ dice.rb
@@ -8,33 +8,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  return "<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset='UTF-8'>
-    <style>
-      ul {
-        list-style: none;
-        padding-left: 0;
-      }
-      ul li::before {
-        content: '🎲';
-        margin-right: 8px;
-        }
-    </style>
-  </head>
-  <body>
-    <h1>Super Fun Dice Roll Extravaganza</h1>
-    <ul>
-      <li><a href=\"/dice/1/6\">1d6</a></li>
-      <li><a href=\"/dice/2/6\">2d6</a></li>
-      <li><a href=\"/dice/1/10\">1d10</a></li>
-      <li><a href=\"/dice/2/10\">2d10</a></li>
-      <li><a href=\"/dice/1/20\">1d20</a></li>
-      <li><a href=\"/dice/2/20\">2d20</a></li>
-    </ul>
-  </body>
-</html>"
+  erb(:homepage)
 end
 
 get("/zebra") do
@@ -47,16 +21,15 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 get("/dice/1/6") do
   die = rand(1..6)
-  outcome = "You rolled a #{die}."
-  "<h1>1d6</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{die}."
+  
+  erb(:one_six)
 end
 get("/dice/1/20") do
   die = rand(1..20)
